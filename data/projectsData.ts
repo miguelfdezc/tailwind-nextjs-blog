c030432301bf6a19235415d7709625523434d3a6
Miguel Fernandez Carratala <miguelfdezc99@gmail.com>
2022-06-26 02:14:19 +0200

    Added initial projects data

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,5 +1,11 @@
 const projectsData = [
   {
+    title: 'App Band Finder',
+    description: `Band Finder es una red social enfocada en la música, cuyo objetivo es hacer más fácil el proceso de poner en contacto miembros para una banda, una jam session (o sesión de improvisación), una simple colaboración o un concierto. La aplicación móvil, permite poner en contacto a gente cuya ubicación sea cercana, indicando posibles intereses por proximidad.`,
+    imgSrc: '/static/images/app-band-finder.png',
+    href: 'http://hdl.handle.net/10045/121389',
+  },
+  /* {
     title: 'A Search Engine',
     description: `What if you could look up any information in the world? Webpages, images, videos
     and more. Google has many features to help you find exactly what you're looking
@@ -14,7 +20,7 @@ const projectsData = [
     forgotten headphones with this simple yet affordable solution.`,
     imgSrc: '/static/images/time-machine.jpg',
     href: '/blog/the-time-machine',
-  },
+  }, */
 ]
 
 export default projectsData
